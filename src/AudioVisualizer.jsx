ad641ecefda2347fb19e6a692e70fb4c02470fe3
q-13t <vova29092002@gmail.com>
2024-06-23 13:02:03 +0300

    UPD: Visualizer behavior

--- src/AudioVisualizer.jsx
+++ src/AudioVisualizer.jsx
@@ -1,3 +1,4 @@
+import { listen } from "@tauri-apps/api/event";
 import { appWindow } from "@tauri-apps/api/window";
 import { useEffect } from "react";
 
@@ -28,44 +29,42 @@ const AudioVisualizer = ({ player }) => {
         let barPosition;
 
         async function draw() { // audio visualization draw loop
+            barPosition = 0;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            analyzer.getByteFrequencyData(dataArray);
+            let color = getComputedStyle(document.body).getPropertyValue("--accent-color");
+            for (let i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i] / 2;
+                context.fillStyle = color;
+                context.fillRect(barPosition, canvas.height - barHeight, barWidth, barHeight);
+                context.fillRect(canvas.width - barPosition, canvas.height - barHeight, barWidth, barHeight);
+                barPosition += barWidth;
+            }
             if (!draw_break) {
-                barPosition = 0;
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                analyzer.getByteFrequencyData(dataArray);
-                let color = getComputedStyle(document.body).getPropertyValue("--accent-color");
-                for (let i = 0; i < bufferLength; i++) {
-                    barHeight = dataArray[i] / 2;
-                    context.fillStyle = color;
-                    context.fillRect(barPosition, canvas.height - barHeight, barWidth, barHeight);
-                    context.fillRect(canvas.width - barPosition, canvas.height - barHeight, barWidth, barHeight);
-                    barPosition += barWidth;
-                }
+                requestAnimationFrame(draw);
             } else {
                 context.clearRect(0, 0, canvas.width, canvas.height);
-
             }
-            requestAnimationFrame(draw);
         }
 
-        let focus = (e) => {
-            draw_break = false;
-        };
-        let blur = (e) => {
-            draw_break = true;
-        };
 
-        let checkVisualizer = () => {
+        let unlisten;
+        let checkVisualizer = async () => {
             const data = localStorage.getItem("visualizer");
-            console.log(`[checkVisualizer] ${data}`);
+
             if (data === "On Window Focus") {
-                window.addEventListener('focus', focus, true);
-                window.addEventListener('blur', blur, true);
-                window.dispatchEvent(new Event('focus'));
+                unlisten = await appWindow.onFocusChanged(({ payload: focused }) => {
+                    focused ? draw_break = false : draw_break = true;
+                    console.log(`[onFocusChanged] focused?= ${focused}, draw_break= ${draw_break}`);
+                    draw();
+                })
+                appWindow.emit('focus', { payload: true });
             } else {
-                window.removeEventListener('focus', focus, true);
-                window.removeEventListener('blur', blur, true);
+                if (unlisten) unlisten();
                 draw_break = data === "Always On" ? false : true;
             }
+            console.log(`[checkVisualizer] mode= ${data}, draw_break= ${draw_break}`);
+            draw();
         };
 
         checkVisualizer();
@@ -73,8 +72,9 @@ const AudioVisualizer = ({ player }) => {
         draw();
         return () => {
             window.removeEventListener('storage', checkVisualizer);
-            window.removeEventListener('focus', focus, true);
-            window.removeEventListener('blur', blur, true);
+            if (unlisten) unlisten();
+            draw_break = true;
+            console.log("[AudioVisualizer] Unloaded");
         };
     }, [])
     return (<canvas id="PlayerControlsCanvas" ></canvas>);
